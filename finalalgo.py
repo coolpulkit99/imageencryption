94f90d2eb5bb770f7747fd98fb12570884378507
Apoorv Goyal <coolpulkit99@gmail.com>
2019-02-18 02:33:36 +0530

    timed actions encrypt and decrypt

--- finalalgo.py
+++ finalalgo.py
@@ -7,7 +7,9 @@ import scipy.signal as sig
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-
+import time
+print("Start encryption")
+timstar=time.time()
 #Encrpytion generating "key1"
 code=pyqrcode.create("number1number2")
 #read key1 and show it
@@ -39,7 +41,7 @@ cv2.imshow('img',(imgkey))
 #read image and show it
 img=cv2.imread("ronaldo.jpg")
 cv2.imshow('img1',img)
-
+#img=cv2.resize(img,(50,50))
 dimen=(np.shape(img))
 print(dimen)
 
@@ -52,21 +54,43 @@ imgkeystraight=np.reshape(imgkey2,(np.size(img)))
 
 #vectorize image
 sign=np.reshape(img,(np.size(img)))
+plt.figure(-1)
+plt.plot(sign)
+
 #Encrypting using key1
 encrypimg=np.int32(sign)*imgkeystraight
+plt.figure(0)
+plt.plot(encrypimg)
 abcc=np.reshape((encrypimg),dimen)
 cv2.imshow('img4',abcc)
 
 #Encrpytion generating "key2"
 key=np.int32(10*np.sin(np.linspace(1,10,10)))
+plt.figure(1)
+plt.plot(key)
 
 #Encrypting using key2
 newsig=sig.convolve(key,encrypimg)
 print("Start")
+plt.figure(2)
+plt.plot(newsig)
 
+timend=time.time()
+print("Encryption finished,time taken=",(timend-timstar))
+
+##plt.show()
 #DEcrypting using key2
 recoveredsig,remain=sig.deconvolve(newsig,key)
+plt.figure(3)
+plt.plot(recoveredsig)
+plt.figure(4)
+plt.plot(remain)
+##plt.show()
+
 print("End")
+
 #DEcrypting using key1
 abbc=np.reshape(np.uint8(np.divide(recoveredsig,imgkeystraight)),dimen)
 cv2.imshow('img3',abbc)
+print("Time taken for decryption=",-timend+time.time())
+plt.show()
